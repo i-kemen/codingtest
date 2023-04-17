9bd014786a76330075ab0d5c00b0a1a3b76eda74
i-kemen <117060656+i-kemen@users.noreply.github.com>
2023-04-17 11:40:48 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17784 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        String[] input = scanner.nextLine().split(" ");
+        System.out.println(input[0] + "??!");
+    }
+}
