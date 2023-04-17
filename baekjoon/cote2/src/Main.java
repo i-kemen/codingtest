0d31d75783e60eeec94b0e68532f3f6768773288
i-kemen <lynshu19@gmail.com>
2023-04-17 10:52:35 +0900

    사칙연산

--- baekjoon/cote2/src/Main.java
+++ baekjoon/cote2/src/Main.java
@@ -6,16 +6,8 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
         String[] input = scanner.nextLine().split(" ");
-        int answer = Integer.parseInt(input[0]) + Integer.parseInt(input[1]);
+        double answer = Double.parseDouble(input[0])/Double.parseDouble(input[1]);
         System.out.println(answer);
 
-
-//        Scanner scanner = new Scanner(System.in);
-//
-//        int A = scanner.nextInt();
-//        int B = scanner.nextInt();
-//
-//
-//        System.out.println(A-B);
     }
 }
