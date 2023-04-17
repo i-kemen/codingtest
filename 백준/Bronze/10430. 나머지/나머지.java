3c86a497de1e36c62f0cc56981275c77ff06a195
i-kemen <117060656+i-kemen@users.noreply.github.com>
2023-04-17 12:11:39 +0900

    [Bronze V] Title: 나머지, Time: 208 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String[] input = scanner.nextLine().split(" ");
+        int A = Integer.parseInt(input[0]);
+        int B = Integer.parseInt(input[1]);
+        int C = Integer.parseInt(input[2]);
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C) * (B%C))%C);
+
+    }
+}
